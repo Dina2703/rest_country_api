d6758d226e65fbdb894328327d836c419d664149
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-02-15 10:44:06 -0500

    CountryDetails page: display selected country details

--- src/pages/CountryDetails.js
+++ src/pages/CountryDetails.js
@@ -28,24 +28,56 @@ function CountryDetails() {
   return (
     country && (
       <div
-        className="card"
+        className="country_details container"
         style={{ backgroundColor: theme.backgroundColor, color: theme.color }}
       >
-        <img className="card_img" src={country.flag} alt="" />
-        <div className="card_body">
-          <h3>{country.name}</h3>
-          <p>
-            <span>Popularion:</span>{" "}
-            {country.population
-              .toString()
-              .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-          </p>
-          <p>
-            <span>Region:</span> {country.region}
-          </p>
-          <p>
-            <span>Capital:</span> {country.capital}
-          </p>
+        <div>
+          <button>Back</button>
+        </div>
+        <div className="flex details_card">
+          <img className="card_img" src={country.flag} alt="" />
+          <div className="card_body">
+            <ul>
+              <li>
+                <h3>{country.name}</h3>
+              </li>
+              <li>
+                <span>Popularion:</span>{" "}
+                {country.population
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+              </li>
+              <li>
+                <span>Region:</span> {country.region}
+              </li>
+              <li>
+                <span>Sub Region:</span> {country.subregion}
+              </li>
+              <li>
+                <span>Capital:</span> {country.capital}
+              </li>
+              <li>
+                <span>Top Level Domain</span>
+                {country.topLevelDomain}
+              </li>
+              <li>
+                <span>Currencies:</span>
+                {country.currencies.map((each) => each.name).join(", ")}
+              </li>
+              <li>
+                <span>Languages:</span>
+                {country.languages.map((each) => each.name).join(", ")}
+              </li>
+            </ul>
+            <div>
+              <span>Border Countries: </span>
+              <ul>
+                <li>{}</li>
+                <li></li>
+                <li></li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     )
