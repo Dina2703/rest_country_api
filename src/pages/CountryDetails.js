fbc7dbe2cc8f5f7818cb5f47d8059168f81408d4
Dinara Idrissova <Dinara.Idrissova@list.ru>
2023-02-15 21:35:36 -0500

    mobile design for the country details page

--- src/pages/CountryDetails.js
+++ src/pages/CountryDetails.js
@@ -57,41 +57,48 @@ function CountryDetails() {
           />
           <div className="card_body">
             <h3>{country.name}</h3>
-            <ul className="country_stats">
-              <li>
-                <span>Native Name:</span> {country.nativeName}
-              </li>
-              <li>
-                <span>Popularion:</span>{" "}
-                {country.population
-                  .toString()
-                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
-              </li>
-              <li>
-                <span>Region:</span> {country.region}
-              </li>
-              <li>
-                <span>Sub Region:</span> {country.subregion}
-              </li>
-              <li>
-                <span>Capital:</span> {country.capital}
-              </li>
-              <li>
-                <span>Top Level Domain</span>
-                {country.topLevelDomain}
-              </li>
-              <li>
-                <span>Currencies:</span>
-                {country.currencies?.map((each) => each.name).join(", ")}
-              </li>
-              <li>
-                <span>Languages:</span>
-                {country.languages?.map((each) => each.name).join(", ")}
-              </li>
-            </ul>
+            <div className="stats">
+              <ul className="country_stats">
+                <li>
+                  <span>Native Name:</span> {country.nativeName}
+                </li>
+                <li>
+                  <span>Popularion:</span>{" "}
+                  {country.population
+                    .toString()
+                    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
+                </li>
+                <li>
+                  <span>Region:</span> {country.region}
+                </li>
+
+                <li>
+                  <span>Sub Region:</span> {country.subregion}
+                </li>
+                <li>
+                  <span>Capital:</span> {country.capital}
+                </li>
+              </ul>
+              <ul className="country_stats part-2">
+                <li>
+                  <span>Top Level Domain</span>
+                  {country.topLevelDomain}
+                </li>
+                <li>
+                  <span>Currencies:</span>
+                  {country.currencies?.map((each) => each.name).join(", ")}
+                </li>
+                <li>
+                  <span>Languages:</span>
+                  {country.languages?.map((each) => each.name).join(", ")}
+                </li>
+              </ul>
+            </div>
+
             <div className="borders">
               <span>Border Countries: </span>
-              <ul>
+
+              <ul className="grid_list">
                 {country.borders?.map((each) => (
                   <li
                     key={each}
